0e4cee5fa1602543be8c73bdfbfa2049c7ae59a8
Rifat <rifat0153@gmail.com>
2020-11-08 15:15:10 +0600

    Tinder Backend

---
+++ server.js
@@ -0,0 +1,51 @@
+import express, { response } from 'express';
+import mongoose from 'mongoose';
+import Cors from 'cors';
+
+import Cards from './dbCards.js'
+
+//App Config
+const app = express();
+const port = process.env.PORT || 8001;
+const connection_url = 'mongodb+srv://admin:papbstfvUliUfkVN@cluster0.ltfnd.mongodb.net/tinderdb?retryWrites=true&w=majority'
+
+// MiddleWare
+app.use(express.json());
+app.use(Cors());
+
+// DB Config
+mongoose.connect(connection_url, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+})
+
+// API Endpoints
+app.get("/", (req, res) => res.status(200).send("Hello Clever Programmers!!"));
+
+app.post("/tinder/cards", (req, res) => {
+    const dbCard = req.body;
+
+    Cards.create(dbCard, (err, data) => {
+        if(err) {
+            response.status(500).send(err);
+        }
+        else{
+            res.status(201).send(data);
+        }
+    });
+});
+
+app.get("/tinder/cards", (req, res) => {
+    Cards.find((err, data) => {
+        if(err) {
+            response.status(500).send(err);
+        }
+        else{
+            res.status(200).send(data);
+        }
+    });
+});
+
+// Listner
+app.listen(port, () => console.log(`listeninig on localhost: ${port}`));
